013501aa76132acd28b97a1526a2427493f987dc
Daniil-Sen <daniil.sen@live.com>
2024-02-23 09:51:50 -0500

    Added tailwindcss code
    I added tailwindcss tsx code to see if tailwind works. It does.

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,12 @@ export default async function IndexPage({
       <Card className="mt-6">
         <UsersTable users={users} />
       </Card>
+      <div className="bg-blue-500 p-4 text-white">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold">Tailwind CSS Works!</h1>
+        <p className="mt-2">This is to check if tailwind css works.</p>
+      </div>
+    </div>
     </main>
   );
 }
