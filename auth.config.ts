ffdccfe98380192b60034bcde0ab73d2d067874a
Louie T <tselouie@gmail.com>
2024-03-13 17:16:09 -0400

    refactor auth objects

---
+++ auth.config.ts
@@ -0,0 +1,39 @@
+import GitHub from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/Credentials';
+import { validateOTP } from '@/lib/otp';
+import type { NextAuthConfig } from 'next-auth';
+
+export default {
+  providers: [
+    GitHub,
+    Credentials({
+      name: 'OTP',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'Enter your Email'
+        },
+        otp: { label: 'OTP', type: 'text', placeholder: 'Enter your OTP' }
+      },
+      async authorize(credentials: Partial<Record<'email' | 'otp', unknown>>) {
+        try {
+          const user = await validateOTP(credentials);
+          if (user) {
+            console.log('auth successful');
+            return user;
+          } else {
+            throw new Error('Invalid OTP');
+          }
+        } catch (error) {
+          throw new Error('Failed to validate OTP');
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      return session;
+    }
+  }
+} satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,9 +1,8 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import authConfig from './auth.config';
+import vercelPostgresAdapter from '@/lib/vercelPostgresAdapter';
 
-export const {
-  handlers: { GET, POST },
-  auth
-} = NextAuth({
-  providers: [GitHub]
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  session: { strategy: 'jwt' },
+  ...authConfig
 });
