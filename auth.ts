92a7341af924f7ccfcc10ddb1195424a60511828
Louie T <tselouie@gmail.com>
2024-03-13 17:21:16 -0400

    remove unused import

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
-import vercelPostgresAdapter from '@/lib/vercelPostgresAdapter';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: 'jwt' },
