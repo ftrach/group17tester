2b61854aa9c3f895cafc032ded42a97babddf6fc
Muhammad Sharif <msharif28@myseneca.ca>
2024-03-22 03:39:55 -0400

    fixed build bug

--- app/aboutUs/page.tsx
+++ app/aboutUs/page.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 const AboutUs = () => {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <div className="text-center">
-        <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">Welcome to G17</h1>
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">Welcome to Group 17</h1>
         <p className="text-lg text-gray-600 mb-8">
-          At Group 17, we're all about style and technology. <br/> We blend the latest in tech with the trendiest streetwear to create a unique experience for our clients.
+          At Group 17, we&apos;re all about style and technology. <br /> We blend the latest in tech with the trendiest streetwear to create a unique experience for our clients.
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -14,7 +16,7 @@ const AboutUs = () => {
             Our mission is simple: to provide the stylish street tech enthusiasts with the coolest gadgets, accessories, and apparel that not only look good but also enhance their tech lifestyle.
           </p>
           <p className="text-lg text-gray-600 mb-4">
-            Whether you're a fashion-forward techie looking for the latest smartwatch, a gadget guru searching for cutting-edge accessories, or simply someone who appreciates the fusion of style and technology, Group 17 is your go-to destination.
+            Whether you&apos;re a fashion-forward techie looking for the latest smartwatch, a gadget guru searching for cutting-edge accessories, or simply someone who appreciates the fusion of style and technology, Group 17 is your go-to destination.
           </p>
         </div>
         <div className="rounded-lg overflow-hidden shadow-lg bg-white p-6">
