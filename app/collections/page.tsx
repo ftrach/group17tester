ae016bc79983a95a4f0679e3b37dce5b3eb445eb
Hamsharan Mahalingam <hamsharan@gmail.com>
2024-03-22 03:14:25 -0400

    finished static landing pages

---
+++ app/collections/page.tsx
@@ -0,0 +1,11 @@
+
+const Collections = () => {
+    return (
+        <main className="p-4 md:p-10 mx-auto max-w-7xl">
+            <h1>Collections</h1>
+            <p>Check out our latest collections!</p>
+        </main>
+    );
+};
+
+export default Collections;
